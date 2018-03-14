83f37c5900084746baa320a988b12fb9a384e097
Go All CashbackCoin <>
2018-03-14 13:06:13 +0530

    initialize code
    This is initial commit

---
+++ build/build.h
@@ -0,0 +1,2 @@
+// No build information available
+#define BUILD_DATE "2018-02-26 03:08:23 +0200"
---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define GACBcoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "GACBcoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
